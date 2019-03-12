7667984aabfe723c4286cd33844506a88e292fe4
venture21 <park0122@gmail.com>
2019-03-12 13:25:41 +0900

    copy test
    open함수를 테스트하기 위한 코드 및 데이터들

---
+++ copy.c
@@ -0,0 +1,32 @@
+// ex) copy a.txt b.txt
+#include <stdio.h>		// printf()
+#include <fcntl.h>		// O_RDONLY, O_WRONLY, O_RDWR
+#include <unistd.h>		// write(), read(), close()
+
+#define BUF_SIZE 1024
+//#define DEBUG 
+
+int main(int argc, char *argv[])
+{
+
+#ifdef DEBUG
+	printf("argc=%d\n", argc);
+	printf("argv[1]=%s\n",argv[1]);
+	printf("argv[2]=%s\n",argv[2]);
+#endif
+
+	int fd1, fd2, len;
+	char buf[BUF_SIZE];
+
+	fd1 = open(argv[1], O_RDONLY); // Open read file
+	fd2 = open(argv[2],O_WRONLY|O_CREAT|O_TRUNC, 0644);  // Open write file
+
+	// read & write
+	while((len=read(fd1, buf, sizeof(buf)))>0)
+			write(fd2,buf,len);
+
+	close(fd1);
+	close(fd2);
+
+	return 0;
+}
