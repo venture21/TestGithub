834d7e97ae4d4680969c3560626df6505ad163b8
venture21 <park0122@gmail.com>
2019-03-25 11:49:25 +0900

    update pthread1.c
    update pthread1.c

--- pthread1.c
+++ pthread1.c
@@ -5,15 +5,27 @@
 //POSIX Thread
 #include <pthread.h>
 
+#define DATASIZE	sizeof(int)*4
+
+int glob_var = 11;
+int *addr;
+
 // Thread function
 void* t_function(void* data)
 {
+	int i;
 	int argData;
 	pthread_t t_id;
 
 	argData = *((int*)data);
 	t_id = pthread_self();
-	printf("getpid=%d, tid=%lu, argData:%d\n", getpid(), t_id, argData);
+	sleep(argData);
+	printf("getpid=%d, tid=%lu, argData:%d\n, glob_var:%d", getpid(), t_id, argData, glob_var);
+	for (i = 0; i < 4; i++)
+	{
+		printf("%d ", *(addr+i));
+	}
+	printf("\n");
 	sleep(5);
 
 	return (void*)(argData*argData);
@@ -21,12 +33,21 @@ void* t_function(void* data)
 
 int main(void)
 {
+
 	//POSIX thread id data type;
 	pthread_t p_thread[2];
+	int i;
 	int ret;
 	int a = 1;
 	int b = 2;
 	int status;
+
+	addr = malloc(DATASIZE);
+
+	for (i = 0; i < 4; i++)
+	{
+		*(addr+i) = i;
+	}
 
 	printf("PID=%d\n", getpid());
 
